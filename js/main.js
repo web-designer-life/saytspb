243944b160d3e13a6cf79257697f8619dce2f515
web-designer-life <io57196657@gmail.com>
2020-04-01 18:21:18 +0300

    feat: media for video

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var menuButton = document.querySelector('.burger__menu-button');
-var menu = document.querySelector('.header__menu');
+var menu = document.querySelector('.header__information');
 
 menuButton.addEventListener('click', function() {
   menuButton.classList.toggle('burger__menu-button--active'); 
-  menu.classList.toggle('header__menu--active'); 
+  menu.classList.toggle('header__information--active'); 
 });
