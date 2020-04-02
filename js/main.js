f8a6f1112870f2d8c3f406c3646f18aa354a3396
web-designer-life <io57196657@gmail.com>
2020-04-02 16:57:58 +0300

    fix: main.js for header

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ menuButton.addEventListener('click', function() {
   menuButton.classList.toggle('burger__menu-button--active'); 
   menu.classList.toggle('header__information--active'); 
 });
+
+$(window).scroll(function(){
+  // 200px от верха
+  if ($(window).scrollTop() > '100'){
+    $('.header__menu').css('background-color', '#e3fbdd');
+    $('.header__menu').css('border-radius', '20px');
+  }
+  if ($(window).scrollTop() < '150'){
+    $('.header__menu').css('background-color', 'transparent');
+  }
+});
