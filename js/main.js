7b1a19b81837badddda606a4f91ca4a340c470ef
web-designer-life <io57196657@gmail.com>
2020-04-03 16:32:07 +0300

    feat: cases and media for cases

--- js/main.js
+++ js/main.js
@@ -18,4 +18,16 @@ $(document).ready(function () {
     }
   });
   
+  $(document).ready(function () {
+  //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
+
 });
