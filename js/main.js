bf1c08ac2142b9019e01b52ea735edafa24d2cd1
web-designer-life <io57196657@gmail.com>
2020-04-03 09:49:00 +0300

    fix: main.js for header

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 var menuButton = document.querySelector('.burger__menu-button');
 var menu = document.querySelector('.header__information');
+$(document).ready(function () {
 
-menuButton.addEventListener('click', function() {
-  menuButton.classList.toggle('burger__menu-button--active'); 
-  menu.classList.toggle('header__information--active'); 
-});
+  menuButton.addEventListener('click', function() {
+    menuButton.classList.toggle('burger__menu-button--active'); 
+    menu.classList.toggle('header__information--active'); 
+  });
 
-$(window).scroll(function(){
-  // 200px от верха
-  if ($(window).scrollTop() > '100'){
-    $('.header__menu').css('background-color', '#e3fbdd');
-    $('.header__menu').css('border-radius', '20px');
-  }
-  if ($(window).scrollTop() < '150'){
-    $('.header__menu').css('background-color', 'transparent');
-  }
+  $(window).scroll(function(){
+    // 200px от верха
+    if ($(window).scrollTop() > '100'){
+      $('.header__menu').css('background-color', '#e3fbdd');
+      $('.header__menu').css('border-radius', '50px');
+    }
+    if ($(window).scrollTop() < '150'){
+      $('.header__menu').css('background-color', 'transparent');
+    }
+  });
+  
 });
