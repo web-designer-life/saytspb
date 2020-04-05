7604d96b8fc294c2e09077e789c6a0684199c5a8
web-designer-life <io57196657@gmail.com>
2020-04-05 14:58:24 +0300

    feat: metrika

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-var {src, dest, watch} = require('gulp');
+var {src, dest, watch, series} = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var minify = require('gulp-minify');
+var htmlmin = require('gulp-htmlmin');
+var tinypng = require('gulp-tinypng-compress');
 
 function bs() {
   serveSass();
@@ -26,4 +30,53 @@ function serveSass() {
     .pipe(browserSync.stream());
 }
 
+function buildCSS(done) {
+  src('css/**/**.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css/'));
+  done();
+}
+
+function buildJS(done) {
+  src(['js/**.js', '!js/**.min.js'])
+    .pipe(minify({
+      ext:{
+        min:'.js'
+      },
+      noSource: true,
+
+    }))
+    .pipe(dest('dist/js'));
+    src('js/**.min.js')
+    .pipe(dest('dist/js'));
+  done();
+}
+
+function html(done) {
+  src('**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+  done();
+}
+
+function php(done) {
+  src('**.php')
+    .pipe(dest('dist/'));
+  src('phpmailer/**/**')
+    .pipe(dest('dist/phpmailer/'));
+  done();
+}
+
+function imagemin(done) {
+  src('img/**/*.{png,jpg,jpeg}')
+    .pipe(tinypng({
+      key: '28bQ8n6HVHMPdMP5Y0L47cxxVS9GCcRD',
+      }))
+    .pipe(dest('dist/img/'));
+  src('img/**/**.{svg,ico}')
+    .pipe(dest('dist/img/'));
+  done();
+}
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, imagemin);
